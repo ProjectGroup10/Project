ddea5ad30482b5e9d8b4209d99033ebea6b53879
FrapperColin <colin.frapper@gmail.com>
2017-04-17 22:58:39 +0200

    Clean working

---
+++ Group10_Project/src/application/Main.java
@@ -0,0 +1,28 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+//Hello
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root1 = FXMLLoader.load(getClass().getResource("MainTemplate.fxml"));
+			Scene scene = new Scene(root1, 1200,800);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
