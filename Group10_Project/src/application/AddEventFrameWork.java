437d54514352711dff0e0795c08bce8a063bc384
nickyleee <nickyleeeeee@gmail.com>
2017-04-27 12:50:21 +0200

    Changes to be committed:  modified:   Group10_Project/src/application/AddEventFrameWork.java

--- Group10_Project/src/application/AddEventFrameWork.java
+++ Group10_Project/src/application/AddEventFrameWork.java
@@ -4,44 +4,57 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.time.LocalDate;
+
 import javax.imageio.ImageIO;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
 import javafx.scene.control.DatePicker;
-import javafx.scene.control.Dialog;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
-
+import javafx.stage.Stage;
+/**
+ * @role This class represent add event dialog
+ * @author Meng Li, Frapper Colin
+ * @date : 04/25/2017
+ */
 public class AddEventFrameWork 
 {
-	private ButtonType submitAddTimeline;
+	private Button submitAddEvent;
 	private DatePicker StartDatePicker;
 	private DatePicker EndDatePicker;
 	private TextField TitleField;
 	private TextArea DescField;
 	private Button image;
 	private Image Addimage;
-
-	private Dialog<Event> dialogue;
+	private Label label;
+	private Stage stage;
 	
-	
+	/**
+	 * Add new event dialog
+	 */
 	public void addtimeline_Page()
 	{
-		dialogue = new Dialog<>();
-		dialogue.setTitle("Add new TimeLine");
-		
-		submitAddTimeline = new ButtonType("Submit",ButtonData.OK_DONE);
-		dialogue.getDialogPane().getButtonTypes().addAll(submitAddTimeline);
-		
+		stage = new Stage();
+		stage.setTitle("Add new Event");
 		GridPane GP = new GridPane();
+		GP.setPadding(new Insets(10,10,10,10));
+		GP.setVgap(5);
+		GP.setHgap(10);
+		
+		Scene scene = new Scene(GP,350,400);
+		stage.setScene(scene);
+		
 		Text title = new Text("Title:");
 		TitleField = new TextField();
 		GP.add(title, 0, 0);
@@ -68,10 +81,20 @@ public class AddEventFrameWork
 		image = new Button("Choose");
 		GP.add(Addimage, 0, 4);
 		GP.add(image, 1, 4);
+		
+		submitAddEvent = new Button("Submit");
+		submitAddEvent.setPrefSize(150, 30);
+		label = new Label();
+		label.setFont(new Font("Sans Serif",18));
+		label.setTextFill(Color.RED);
+		GP.add(label, 1, 5);
+		GP.add(submitAddEvent,1,6);
 		setImageButton();
-	    dialogue.getDialogPane().setContent(GP);
+		stage.show();
 	}
-	
+	/**
+	 * set image
+	 */
 	public void setImageButton()
 	{
 		   image.setOnAction(new EventHandler<ActionEvent>(){
@@ -81,10 +104,14 @@ public class AddEventFrameWork
 				// TODO Auto-generated method stub
 				FileChooser fileChooser = new FileChooser();
 	             
-	            FileChooser.ExtensionFilter extFilterJPG = new FileChooser.ExtensionFilter("JPG files (*.jpg)", "*.JPG");
-	            FileChooser.ExtensionFilter extFilterPNG = new FileChooser.ExtensionFilter("PNG files (*.png)", "*.PNG");
-	            fileChooser.getExtensionFilters().addAll(extFilterJPG, extFilterPNG);
-	              
+				FileChooser.ExtensionFilter extFilterJPG = 
+		                    new FileChooser.ExtensionFilter("JPG files (*.JPG)", "*.JPG");
+		        FileChooser.ExtensionFilter extFilterjpg = 
+		                    new FileChooser.ExtensionFilter("jpg files (*.jpg)", "*.jpg");
+		        FileChooser.ExtensionFilter extFilterpng = 
+		                    new FileChooser.ExtensionFilter("png files (*.png)", "*.png");
+		            fileChooser.getExtensionFilters()
+		                    .addAll(extFilterJPG, extFilterjpg, extFilterpng);
 	            File file = fileChooser.showOpenDialog(null);
 	                       
 	            try {
@@ -95,41 +122,72 @@ public class AddEventFrameWork
 			}});
 	}
 	
-	
-	public ButtonType getbuttonType()
+	/**
+	 * Getter
+	 * @return submitAddTimeline
+	 */
+	public Button getbuttonType()
 	{
-		return submitAddTimeline;
+		return submitAddEvent;
 	}
-	public Dialog<Event> getDialog()
+	/**
+	 * Getter
+	 * @return stage
+	 */
+	public Stage getStage()
 	{
-		return dialogue;
+		return stage;
 	}
-
+	/**
+	 * Getter
+	 * @return true or false
+	 */
 	public boolean IsDuration()
 	{
-		if(!(EndDatePicker.getValue().getMonth().equals(StartDatePicker.getValue().getMonth())))
+		if(EndDatePicker.getValue().equals(StartDatePicker.getValue()))
 		{
-			return true;
+			return false;
 		}
 		else
 		{
-			if(EndDatePicker.getValue().getDayOfMonth() - StartDatePicker.getValue().getDayOfMonth() != 0)
-			{
-				return true;
-			}
-			else
-			{
-				return false;
-			}
+			return true;
 		}
 		
 	}
+	/**
+	 * Getter
+	 * @return TitleField
+	 */
+	public TextField getTextfield()
+	{
+		return TitleField ;
+	}
+	/**
+	 * Getter
+	 * @return label
+	 */
+	public Label getLabel()
+	{
+		return label;
+	}
+	/**
+	 * Getter
+	 * @return StartDatePicker
+	 */
 	public DatePicker getStartDateper(){
 		return StartDatePicker;
 	}
+	/**
+	 * Getter
+	 * @return EndDatePicker
+	 */
 	public DatePicker getEndDateper(){
 		return EndDatePicker;
 	}
+	/**
+	 * Getter
+	 * @return event
+	 */
 	public Event getEvent()
 	{
 		
